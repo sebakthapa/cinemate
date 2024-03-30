38d81a950e70b25359ad4e45e44a875368e0d170
SebakThapa <thapasebak059@gmail.com>
2024-03-30 11:19:00 +0545

    Some error solved

--- src/app/api/profile/[uid]/route.js
+++ src/app/api/profile/[uid]/route.js
@@ -1,106 +1,113 @@
 // POST => CREATE NEW PROFILE for the user
 
-import connectToDatabase from "@/lib/dbConnect";
-import Profiles from "@/models/profile";
-import { NextResponse } from "next/server";
-import bcrypt from "bcrypt"
+import connectToDatabase from '@/lib/dbConnect';
+import Profiles from '@/models/profile';
+import { NextResponse } from 'next/server';
+import bcrypt from 'bcrypt';
 
 export const POST = async (request) => {
-    try {
-        const saltRounds = 10;
+  try {
+    const saltRounds = 10;
 
-        const query = request.url.split("/");
-        const uid = query[query.length - 1];
+    const query = request.url.split('/');
+    const uid = query[query.length - 1];
 
-        const { name, isKid, avatar, hasPin, pin } = await request.json();
-        let hashedPin;
+    const { name, isKid, avatar, hasPin, pin } = await request.json();
+    let hashedPin;
 
-
-
-
-        // validation part 
-        if (!name && (!pin && hasPin)) {
-            return new NextResponse(JSON.stringify({ message: "Check the box below or create PIN", fields: ["name", "pin"] }), { status: 401 })
-        }
-
-        if (!name) {
-            return new NextResponse(JSON.stringify({ message: "Name is required", fields: ["name"] }), { status: 401 })
-        }
-        if (!pin && hasPin) {
-            return new NextResponse(JSON.stringify({ message: "Click on no PIN below or enter PIN", fields: ["pin"] }), { status: 401 })
-        }
-
-
-
-
-
-        // DB query
-        await connectToDatabase();
-
-        const allProfiles = await Profiles.find({ uid });
-        if (allProfiles.length >= 3) {
-            return new NextResponse(JSON.stringify({ message: "Maximum profile limit reached", fields: ["name"] }), { status: 401 })
-        }
-        const existingProfile = allProfiles.filter((profile) => profile.name == name)
-        if (existingProfile.length > 0) {
-            return new NextResponse(JSON.stringify({ message: "Enter another name", fields: ["name"] }), { status: 401 })
-        }
-
-
-        // hashing pin
-        if (pin) {
-            const hash = await bcrypt.hash(pin, saltRounds)
-            if (!hash) {
-                return new NextResponse(JSON.stringify({ message: "Unable to proceed due to server error. Error in password storage", fields: ["password"] }), { status: 401 })
-            }
-            hashedPin = hash;
-        }
-
-        const newProfile = new Profiles({ uid, name, isKid, avatar, hasPin, pin: hashedPin });
-        // hashedPin && (newProfile.pin = hashedPin)
-        const savedProfile = await newProfile.save();
-
-        return new NextResponse(JSON.stringify({ id: savedProfile._id, uid: savedProfile.uid, name: savedProfile.name, isKid: savedProfile.isKid, avatar: savedProfile.avatar, hasPin: savedProfile.hasPin }))
-
-
-
-
-    } catch (error) {
-        console.log("Error Creating new profile", + error);
-        // return new NextResponse(JSON.stringify({ error: "Error occured while creating new user", details: {...error} }))
+    // validation part
+    if (!name && !pin && hasPin) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Check the box below or create PIN', fields: ['name', 'pin'] }),
+        { status: 401 }
+      );
     }
 
+    if (!name) {
+      return new NextResponse(JSON.stringify({ message: 'Name is required', fields: ['name'] }), {
+        status: 401,
+      });
+    }
+    if (!pin && hasPin) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Click on no PIN below or enter PIN', fields: ['pin'] }),
+        { status: 401 }
+      );
+    }
 
+    // DB query
+    await connectToDatabase();
 
+    const allProfiles = await Profiles.find({ uid });
+    console.group(allProfiles);
+    if (allProfiles.length >= 3) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Maximum profile limit reached', fields: ['name'] }),
+        { status: 401 }
+      );
+    }
+    const existingProfile = allProfiles.filter((profile) => profile.name == name);
+    if (existingProfile.length > 0) {
+      return new NextResponse(JSON.stringify({ message: 'Enter another name', fields: ['name'] }), {
+        status: 401,
+      });
+    }
 
-}
+    // hashing pin
+    if (pin) {
+      const hash = await bcrypt.hash(pin, saltRounds);
+      if (!hash) {
+        return new NextResponse(
+          JSON.stringify({
+            message: 'Unable to proceed due to server error. Error in password storage',
+            fields: ['password'],
+          }),
+          { status: 401 }
+        );
+      }
+      hashedPin = hash;
+    }
 
+    const newProfile = new Profiles({ uid, name, isKid, avatar, hasPin, pin: hashedPin });
+    // hashedPin && (newProfile.pin = hashedPin)
+    const savedProfile = await newProfile.save();
 
-
-
+    return new NextResponse(
+      JSON.stringify({
+        id: savedProfile._id,
+        uid: savedProfile.uid,
+        name: savedProfile.name,
+        isKid: savedProfile.isKid,
+        avatar: savedProfile.avatar,
+        hasPin: savedProfile.hasPin,
+      })
+    );
+  } catch (error) {
+    console.log('Error Creating new profile', +error);
+    // return new NextResponse(JSON.stringify({ error: "Error occured while creating new user", details: {...error} }))
+  }
+};
 
 // GET => get all the profiles of the user
 
 export const GET = async (request) => {
-    try {
-        const query = request.url.split("/");
-        const uid = query[query.length - 1];
+  try {
+    const query = request.url.split('/');
+    const uid = query[query.length - 1];
 
-        // DB query
-        await connectToDatabase();
+    // DB query
+    await connectToDatabase();
 
-        const fetchedProfiles = await Profiles.find({ uid })
+    const fetchedProfiles = await Profiles.find({ uid });
+    console.log(fetchedProfiles);
 
-        const profiles = fetchedProfiles.map(({ _doc: doc }) => {
-            const { pin, ...data } = doc;
-            return data;
-        })
+    const profiles = fetchedProfiles.map(({ _doc: doc }) => {
+      const { pin, ...data } = doc;
+      return data;
+    });
 
-
-        return new NextResponse(JSON.stringify(profiles))
-
-
-    } catch (error) {
-        console.log("Error fetching profiles" + error)
-    }
-}
+    return new NextResponse(JSON.stringify(profiles));
+  } catch (error) {
+    console.log('Error fetching profiles' + error);
+  }
+};
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,78 +1,80 @@
-import { useRouter } from "next/navigation";
-import styles from "./css/profile.module.css"
-import { useDispatch } from 'react-redux'
-import Link from "next/link";
-import Image from "next/image";
-import { loginProfile } from "@/redux/profileSlice";
-import { BsThreeDotsVertical } from "react-icons/bs"
-import { AiOutlineUserDelete } from "react-icons/ai";
-import { TbUserEdit } from "react-icons/tb";
-import ConfirmPasswordModal from "./ConfirmPasswordModal";
+import { useRouter } from 'next/navigation';
+import styles from './css/profile.module.css';
+import { useDispatch } from 'react-redux';
+import Link from 'next/link';
+import Image from 'next/image';
+import { loginProfile } from '@/redux/profileSlice';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import { AiOutlineUserDelete } from 'react-icons/ai';
+import { TbUserEdit } from 'react-icons/tb';
+import ConfirmPasswordModal from './ConfirmPasswordModal';
 
 function Profile(props) {
-    const dispatch = useDispatch();
-    const router = useRouter();
+  const dispatch = useDispatch();
+  const router = useRouter();
 
-    const handleClick = () => {
-        if (!props.hasPin) {
-            dispatch(loginProfile(props))
-            // router.push("/home")
-        }
+  const handleClick = () => {
+    if (!props.hasPin) {
+      dispatch(loginProfile(props));
+      // router.push("/home")
     }
+  };
 
-    let linkPath = "#";
+  let linkPath = '#';
 
-
-    const handleRoute = () => {
-        if (props.type == "addProfile") {
-            router.replace("/profiles/create")
-        } else {
-            if (props.hasPin) {
-                router.replace(`/profiles/login/${props.name}`)
-            } else {
-                router.replace("/home")
-            }
-        }
+  const handleRoute = () => {
+    if (props.type == 'addProfile') {
+      router.replace('/profiles/create');
+    } else {
+      if (props.hasPin) {
+        router.replace(`/profiles/login/${props.name}`);
+      } else {
+        handleClick();
+        router.replace('/home');
+      }
     }
+  };
 
-
-    return (
-        <div className={styles.profile} >
-            {/* <ConfirmPasswordModal /> */}
-            {
-                props.type !== "addProfile" && (
-                    <div className={styles.menu}>
-                        <button className={styles.menuTrigger}>
-                            <BsThreeDotsVertical style={{ float: "right" }} />
-                        </button>
-                        <div className={styles.menuOptions}>
-                            <ul className={""}>
-                                <li style={{ color: "red" }}><AiOutlineUserDelete color="red" /> Delete</li>
-                                <li style={{ color: "green" }}><TbUserEdit color="green" /> Edit</li>
-                            </ul>
-                        </div>
-                    </div>
-                )
-            }
-            <div>
-                {
-                    props.type != "addProfile" && (
-                        <div onClick={handleRoute} style={{ height: "150px", "width": "150px", cursor: "pointer" }} dangerouslySetInnerHTML={{ __html: props.avatar }} className={styles.profile__svg}></div>
-                    )
-                }
-                {
-                    props.type == "addProfile" && (
-                        <div className={styles.profile__image} onClick={handleRoute} >
-                            <Image height="150" src={props.avatar} alt="profile avatar" width="150" />
-                        </div>
-                    )
-                }
-                <div className={styles.profile__text} onClick={handleRoute}>
-                    <h3>{props.name}</h3>
-                </div>
-            </div>
+  return (
+    <div className={styles.profile}>
+      {/* <ConfirmPasswordModal /> */}
+      {props.type !== 'addProfile' && (
+        <div className={styles.menu}>
+          <button className={styles.menuTrigger}>
+            <BsThreeDotsVertical style={{ float: 'right' }} />
+          </button>
+          <div className={styles.menuOptions}>
+            <ul className={''}>
+              <li style={{ color: 'red' }}>
+                <AiOutlineUserDelete color='red' /> Delete
+              </li>
+              <li style={{ color: 'green' }}>
+                <TbUserEdit color='green' /> Edit
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+      )}
+      <div>
+        {props.type != 'addProfile' && (
+          <div
+            onClick={handleRoute}
+            style={{ height: '150px', width: '150px', cursor: 'pointer' }}
+            dangerouslySetInnerHTML={{ __html: props.avatar }}
+            className={styles.profile__svg}
+          ></div>
+        )}
+        {props.type == 'addProfile' && (
+          <div className={styles.profile__image} onClick={handleRoute}>
+            <Image height='150' src={props.avatar} alt='profile avatar' width='150' />
+          </div>
+        )}
+        <div className={styles.profile__text} onClick={handleRoute}>
+          <h3>{props.name}</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
